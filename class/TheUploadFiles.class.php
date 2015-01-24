868b9146bc919bfa4af54b754d60235ea681a4c8
valdiney <valdiney.2@hotmail.com>
2015-01-24 20:39:41 -0200

    fisrt commit

---
+++ class/TheUploadFiles.class.php
@@ -0,0 +1,94 @@
+<?php 
+/**
+* Class para fazer Upload de pequenos arquivos...
+* @var $config - array de possíveis erros que podem ocorrer
+* @var $file - array $_FILES contendo o nome do campo file
+* @var $extensios - array contendo os tipos dos arquivos que você deseja permitir
+* @version 0.1
+* @author Valdiney França
+*/
+
+class TheUploadFiles
+{
+	private $config;
+	private $file;
+	private $extensions;
+    
+    /*set - Seta os atributos*/
+	public function setFile($file)
+	{
+		$this->file = $file;
+	}
+
+	public function sendTo($folder)
+	{
+		$this->config["folder"] = $folder;
+	}
+
+	public function setExtensions($extensions)
+	{
+		$this->extensions = $extensions;
+	}
+    /*end set*/
+
+    /*Este método efetua um conjunto de validações*/
+	private function configAndSendUploads()
+	{
+		$this->config["fileLength"] = 1024 * 1024 * 2;
+		$this->config["theExtensions"] = $this->extensions;
+		$this->config["rename"] = false;
+
+        if ($this->file["error"] != 0)
+        {
+            switch($this->file["error"])
+            {
+                case 1 :
+                    echo "O arquivo é maior do que o permitido pelo PHP";
+                    break;
+                case 2 :
+                    echo "O arquivo ultrapassa o tamanho limite";
+                    break;
+                case 3 :
+                    echo "O arquivo foi carregado parcialmente";
+                    break;
+                case 4 :
+                    echo "Não foi feito o upload do arquivo";
+                    break;
+            }
+        }
+
+        $prepareExtensions = explode(".", $this->file["name"]);
+        $prepareExtensions = strtolower(end($prepareExtensions));
+        if (array_search($prepareExtensions, $this->config["theExtensions"]) === false)
+        {
+        	$typeFiles = null;
+        	foreach($this->config["theExtensions"] as $list)
+        	{
+        		$typeFiles .= $list . ", ";
+        	}
+
+        	echo  "Evie apenas esses tipos de arquivos: " . $typeFiles;
+            exit;
+        }
+        elseif ($this->config["fileLength"] < $this->file["size"])
+        {
+        	echo 'O arquivo enviado é muito grande envie arquivos de até 2Mb';
+        }
+	}
+    
+    /*Este método move os arquivos da pasta temporária para a pasta de destino*/
+	public function sendUpload()
+	{
+		$this->configAndSendUploads();
+		return move_uploaded_file($this->file["tmp_name"], $this->config["folder"] . time() . $this->file["name"]);
+	}
+    
+    /*Descarregam os atributos, associado aos mesmos um valor nulo*/
+	public function __destruct()
+	{
+		$this->file = null;
+		$this->config["folder"] = null;
+		$this->extensions = null;
+	}
+}
+
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+require_once("class/TheUploadFiles.class.php");
+
+if (isset($_GET["enviar"]))
+{
+    $up = new TheUploadFiles();
+    $up->setFile($_FILES["arquivo"]);
+    $up->sendTo("arquivos/");
+    $extencaos = array("jpg","png","gif","pdf","doc","docx","html","txt");
+    $up->setExtensions($extencaos);
+    if ($up->sendUpload())
+    {
+        echo "Arquivo enviado com Sucesso";
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Teste de uso da Class TheUploadFiles</title>
+</head>
+
+<body>
+<form method="post" action="index.php?enviar" enctype="multipart/form-data">
+    <input type="file" name="arquivo">
+    <button type="submit">Enviar</button>
+</form>
+</body>
+
+</html>

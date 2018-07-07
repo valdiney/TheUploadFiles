a5fe172f185c1e4fdff17212ba962409491d9d63
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2018-07-07 07:37:44 -0300

    Correct to new model
    - change $_GET['enviar'] to $_POST['enviar']
    - add a redirect if is not isset
    - add switch to getErrors
    - correct try to verify getErrors

--- save-files.php
+++ save-files.php
@@ -4,43 +4,47 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once("upfiles/UploadFiles.php");
-
-if (isset($_GET["enviar"]))
+if (!isset($_POST["enviar"]))
 {
-    $up = new upfiles\UploadFiles();
-
-    $up->file($_FILES["arquivo"]);
-    $up->folder("arquivos/");
-    $up->maxSize(4);
-    $up->extensions(array("png","jpg","jpeg","gif","pdf","doc","docx","html","txt","avi","mp4", "zip"));
-
-    if ($up->getErrors() == 1) {
-        echo "Formato não esperado";
-        return false;
-    }
-
-    if ($up->getErrors() == 2) {
-        echo "O tamanho limite para Upload é de 4MB";
-        return false;
-    }
-
-    if ($up->getErrors() == 3) {
-        echo "Formato não identificado. Por favor, tente novamente.";
-        return false;
-    }
-
-    if ($up->getErrors() == 4) {
-        echo "Erro interno. Diretório não encontrado.";
-        return false;
-    }
-
-    try {
-        $up->move();
-        echo "Upload feito com Sucesso!";
-    } catch(\Exception $e) {
-        echo "Ocorreu um erro ao tentar fazer o Upload: " . $e->getMessage();
-    }
-
+    header('Location: index.php');
+    exit();
 }
-?>
+
+require_once("upfiles/UploadFiles.php");
+$up = new upfiles\UploadFiles();
+
+$up->file($_FILES["arquivo"]);
+$up->folder("arquivos/");
+$up->maxSize(4);
+$up->extensions(array("png","jpg","jpeg","gif","pdf","doc","docx","html","txt","avi","mp4", "zip"));
+
+$error = null;
+
+switch($up->getErrors()) {
+    case 1:
+        $error = "Formato não esperado";
+        break;
+    case 2:
+        $error = "O tamanho limite para Upload é de 4MB";
+        break;
+    case 3:
+        $error = "Formato não identificado. Por favor, tente novamente.";
+        break;
+    case 4:
+        $error = "Erro interno. Diretório não encontrado.";
+        break;
+}
+
+try {
+    if ($error!==null) {
+        throw new Exception($error);
+    }
+    $up->move();
+    echo "Upload feito com Sucesso! <br>";
+    echo "<a href=\"{$up->destinationPath()}\">Caminho do arquivo.</a>";
+} catch(\Exception $e) {
+    echo "Ocorreu um erro ao tentar fazer o Upload: " . $e->getMessage();
+} finally {
+    echo "<br>";
+    echo "<a href=\"index.php\">Enviar outro arquivo.</a>";
+}

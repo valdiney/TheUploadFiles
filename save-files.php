39148f3248d1cc8212267c5515d344d2831195b9
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2018-07-07 07:16:55 -0300

    Display error in save-files.php

--- save-files.php
+++ save-files.php
@@ -1,4 +1,9 @@
 <?php
+// displaying errors in PHP
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once("upfiles/UploadFiles.php");
 
 if (isset($_GET["enviar"]))
